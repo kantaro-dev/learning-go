f564228b35622e8d82cda6744a3aaa058f3f5b0c
kantaro <kantarodev@gmail.com>
2022-05-18 09:13:40 +0900

    data structures of Maps and Slices

--- main.go
+++ main.go
@@ -2,24 +2,66 @@ package main
 
 import "log"
 
-type myStruct struct {
-	FirstName string
-}
+// import "sort"
 
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
+// type User struct {
+// 	FirstName string
+// 	LastName  string
+// }
 
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "John"
+	// myMap := make(map[string]string)
 
-	myVar2 := myStruct{
-		FirstName: "Mary",
-	}
+	// myMap["dog"] = "Samson"
+	// myMap["other-dog"] = "Cassie"
+	// myMap["dog"] = "Fido"
 
-	// log.Println("myVar is set to", myVar.FirstName)
-	// log.Println("myVar2 is set to", myVar2.FirstName)
-	log.Println("myVar is set to", myVar.printFirstName())
-	log.Println("myVar2 is set to", myVar2.printFirstName())
+	// log.Println(myMap["dog"])
+	// log.Println(myMap["other-dog"])
+
+	// myMap := make(map[string]int)
+
+	// myMap["First"] = 1
+	// myMap["Second"] = 2
+
+	// log.Println(myMap["First"], myMap["Second"])
+
+	// myMap := make(map[string]User)
+
+	// me := User{
+	// 	FirstName: "Rene",
+	// 	LastName:  "Descartes",
+	// }
+
+	// myMap["me"] = me
+	// log.Println(myMap["me"].FirstName)
+
+	// var mySlice []string
+
+	// mySlice = append(mySlice, "Rene")
+	// mySlice = append(mySlice, "George")
+	// mySlice = append(mySlice, "Alfred")
+
+	// log.Println(mySlice)
+
+	// var mySliceInt []int
+
+	// mySliceInt = append(mySliceInt, 2)
+	// mySliceInt = append(mySliceInt, 1)
+	// mySliceInt = append(mySliceInt, 3)
+
+	// sort.Ints(mySliceInt)
+
+	// log.Println(mySliceInt)
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	log.Println(numbers)
+
+	log.Println(numbers[0:2])
+	log.Println(numbers[6:9])
+
+	names := []string{"one", "seven", "fish", "cat"}
+
+	log.Println(names)
 }
