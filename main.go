1a45e8355eb8d98fce57e2299946055e79d66067
kantaro <kantarodev@gmail.com>
2022-05-23 09:25:23 +0900

    packages

--- main.go
+++ main.go
@@ -1,57 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+	"learning-go/helpers"
+)
 
 func main() {
-	dog := Dog{
-		Name:  "Samson",
-		Breed: "German Shephered",
-	}
+	log.Println("Hello")
 
-	PrintInfo(&dog)
-
-	gorilla := Gorilla{
-		Name:          "Jock",
-		Color:         "gray",
-		NumberOfTeeth: 38,
-	}
-
-	PrintInfo(&gorilla)
-
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
-}
-
-func (d *Dog) Says() string {
-	return "Woof"
-}
-
-func (d *Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (g *Gorilla) Says() string {
-	return "Ugh"
-}
-
-func (g *Gorilla) NumberOfLegs() int {
-	return 2
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	log.Println(myVar.TypeName)
 }
